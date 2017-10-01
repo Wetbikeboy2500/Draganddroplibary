96971ecd20b57f29e1d9d31e374190c7db0098f7
Matt <wetbikeboy2500@gmail.com>
2017-10-01 11:24:46 -0400

    code

---
+++ lib.js
@@ -0,0 +1,62 @@
+let mx, my;
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("loading");
+    let dom = document.getElementsByClassName("draggable");
+    for (let i = 0; i < dom.length; i++) {
+        new dom_object(dom.item(i), 0);
+    }
+}, false);
+document.addEventListener("mousemove", (event) => {
+    mx = event.pageX;
+    my = event.pageY;
+}, false);
+class dom_object {
+    constructor (dom, index) {
+        this.dom = dom;//this will refer to the object
+        this.index = index;//this will be its position in the array
+        this.x = this.dom.getBoundingClientRect().left;
+        this.y = this.dom.getBoundingClientRect().top;
+        this.dom.addEventListener("mousedown", () => {
+            this.move();
+        }, false);
+        document.addEventListener("mouseup", () => {
+            this.stop();
+        }, false);
+    }
+    
+    setup () {
+        this.sy = my;
+        this.sx = mx;
+    }
+
+    update () {
+        this.y += (my - this.sy);
+        this.x += (mx - this.sx);
+        this.render();
+    }
+    
+    render () {
+        console.log(this.y);
+        this.dom.style.position = "absolute";
+        this.dom.style.top = this.y + "px";
+        this.dom.style.left = this.x + "px";
+    }
+    
+    move () {
+        this.setup();
+        clearInterval(this.clock);
+        this.clock = setInterval ( () => {
+            this.update();
+            this.setup();
+        });
+    }
+
+    stop () {
+        clearInterval(this.clock);
+    }
+    
+    del () {
+        this.dom.style.pointerEvents = "none";
+        this.dom.parentNode.removeChild(this.dom);
+    }
+}
